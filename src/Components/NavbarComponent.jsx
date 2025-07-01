aaa007f2211bfc20f1e388dd729b36f7f93e9b94
Chetan Fegade <fegadechetan1234@gmail.com>
2025-07-01 19:04:04 +0530

    Fix nested anchor tags in NavbarComponent

--- src/Components/NavbarComponent.jsx
+++ src/Components/NavbarComponent.jsx
@@ -1,45 +1,40 @@
-  import { useState } from 'react';
-  import { Link } from 'react-router-dom';
-  import {
-    Navbar,
-    NavbarBrand,
-    NavbarToggler,
-    Collapse,
-    Nav,
-    NavItem,
-    NavLink
-  } from 'reactstrap';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem
+} from 'reactstrap';
 
-  function NavbarComponent() {
-    const [isOpen, setIsOpen] = useState(false);
+function NavbarComponent() {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => setIsOpen(!isOpen);
+  const toggle = () => setIsOpen(!isOpen);
 
-    return (
-      <Navbar color="dark" dark expand="md" container="md">
-        <NavbarBrand>Employee Manager</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="ms-auto" navbar>
-          
-            <NavItem>
-            <NavLink>
-              <Link to={"/"}  style={{ textDecoration: 'none', color: 'white' }}
-  onMouseEnter={e => (e.target.style.color = '#ccc')}
-  onMouseLeave={e => (e.target.style.color = 'white')}>Home</Link>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink>
-              <Link to={"/add"} style={{ textDecoration: 'none', color: 'white' }}
-  onMouseEnter={e => (e.target.style.color = '#ccc')}
-  onMouseLeave={e => (e.target.style.color = 'white')}>Add Employee</Link>
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    );
-  }
+  return (
+    <Navbar color="dark" dark expand="md" container="md">
+      <NavbarBrand>Employee Manager</NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ms-auto" navbar>
+        
+          <NavItem>
+            <Link to={"/"} style={{ textDecoration: 'none', color: 'white' }}
+              onMouseEnter={e => (e.target.style.color = '#ccc')}
+              onMouseLeave={e => (e.target.style.color = 'white')}>Home</Link>
+          </NavItem>
+          <NavItem>
+            <Link to={"/add"} style={{ textDecoration: 'none', color: 'white' }}
+              onMouseEnter={e => (e.target.style.color = '#ccc')}
+              onMouseLeave={e => (e.target.style.color = 'white')}>Add Employee</Link>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+}
 
-  export default NavbarComponent;
+export default NavbarComponent;
